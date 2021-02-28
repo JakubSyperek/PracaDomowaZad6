5b5954de5542de4656ab2466a3ccc0cf558dbe05
JakubSyperek <162420@student.uwm.edu.pl>
2021-02-28 19:36:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+string = "La la, la la la la la na na na na na, La la na na, la la la la la na na na na na La la, la la la la la na na na na na, La la na na, la la la la la na na na na na"
+substring = "la"
+
+count = string.count(substring)
+
+print("Ilość 'la' w piosence:",count)
